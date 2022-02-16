99477575cadcd62d3b6c2070f33c9b9c2991d3f8
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2022-02-16 08:00:13 -0300

    Realizado validação para quando não encontra a cidade ou localização

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ function App() {
     const [query, setQuery] = useState('');
     const [weather, setWeather] = useState({});
 
-    useEffect(()=> {
-        navigator.geolocation.getCurrentPosition((position)=> {
-        const { latitude, longitude } = position.coords;
-        const searchGeo = async (e) => {
-            const data = await fetchWeather(false, latitude, longitude);
-            setWeather(data);
-            setLocation(true);
-        }
-        searchGeo();
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords;
+            const searchGeo = async (e) => {
+                const data = await fetchWeather(false, latitude, longitude);
+                setWeather(data);
+                setLocation(true);
+            }
+            searchGeo();
         });
 
     }, [])
 
     const search = async (e) => {
-        if(e.key === 'Enter') {
+        if (e.key === 'Enter') {
             const data = await fetchWeather(query);
             setWeather(data);
             setQuery('');
@@ -29,34 +29,38 @@ function App() {
     }
 
 
-  return (
-    <Fragment>
-        <div className="box">
-            <div className="container">
-            <span className="form-search">
-                <input type="text"className="search" placeholder="Buscar" value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search}/>
-            </span>
-            
-                {weather.main && (
-                    <div className="box-city">
-                        <h2 className="city-name">
-                            <span>{weather.name}</span>
-                            <sup>{weather.sys.country}</sup>
-                        </h2>
-                        <div className="city-temp">
-                            {Math.round(weather.main.temp)}
-                            <sup>&deg;C</sup>
+    return (
+        <Fragment>
+            <div className="box">
+                <div className="container">
+                    <span className="form-search">
+                        <input type="text" className="search" placeholder="Buscar" value={query} onChange={(e) => setQuery(e.target.value)} onKeyPress={search} />
+                    </span>
+                    {weather.cod == 404 && (
+                        <p>
+                            Cidade ou localização não encontrado
+                        </p>
+                    )}
+                    {weather.main && (
+                        <div className="box-city">
+                            <h2 className="city-name">
+                                <span>{weather.name}</span>
+                                <sup>{weather.sys.country}</sup>
+                            </h2>
+                            <div className="city-temp">
+                                {Math.round(weather.main.temp)}
+                                <sup>&deg;C</sup>
+                            </div>
+                            <div className="info">
+                                <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                                <p>{weather.weather[0].description}</p>
+                            </div>
                         </div>
-                        <div className="info">
-                            <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-                            <p>{weather.weather[0].description}</p>
-                        </div>
-                    </div>
-                )}
+                    )}
+                </div>
             </div>
-        </div>
-    </Fragment>
-  );
+        </Fragment>
+    );
 }
 
 export default App;
--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -5,10 +5,10 @@ const KEY_API = '09631acb887faa8b86e49b18b006969d';
 
 export const fetchWeather = async (query = false, lat, long) => {
     if (query) {
-        var params = {q: query,appid: KEY_API,lang: 'pt',units: 'metric'}
+        var params = { q: query, appid: KEY_API, lang: 'pt', units: 'metric' }
     } else {
-        var params = {lat: lat,lon: long,appid: KEY_API,lang: 'pt',units: 'metric'}
+        var params = { lat: lat, lon: long, appid: KEY_API, lang: 'pt', units: 'metric' }
     }
-    const {data} = await axios.get(BASE_URL,{params: params});
+    const { data } = await axios.get(BASE_URL, { params: params });
     return data;
 }
